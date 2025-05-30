d571754e615a6dd00e63ab02f375b9253d392190
Sevmamra <sevmamra01@gmail.com>
2025-05-30 09:53:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import datetime
 class Config:
     BOT_TOKEN = os.environ.get('7652800795:AAG02nD1EcssCfSoXsoT2anEjHnv71PiN5U')
     API_HASH = os.environ.get('9699262c708c2e45ba18bfce925ed5ed')
-    API_ID = int(os.environ.get('26797881'))
+    API_ID = int(os.environ.get('26797881')
     OWNER_ID = int(os.environ.get('6567162029')) #Admin UserId
     DB_URI = os.environ.get('mongodb+srv://sevmamra01:<db_password>@cluster0.ff29e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') #May Be In Future.
     
